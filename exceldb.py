d167fe86bf6eb7449c47a04a875465c164c60397
Mono <mono@MonoBook.local>
2018-10-12 17:50:43 +0530

    Test script to read the data and parse it

--- exceldb.py
+++ exceldb.py
@@ -1,20 +1,40 @@
-from openpyxl import load_workbook
-import openpyxl
-import csv
 
-wb = load_workbook(file_name='C:\\Users\sauravkhandelwal\Desktop\Scraper\LinksSAP.xlsx')
+# This program will be used to read data from the sheet and uploaded to the database with a proper schema
+import xlrd, re, pymongo
+from pymongo import MongoClient
 
-sheetnames = wb.get_sheet_names()
-sheet_value_arr = []
+def upload(brand, model, size, smart,url):
+	client = MongoClient('127.0.0.1',27017)
+	db = client['price_tracker']
+	collection = db['tv']
+	doc = ({"brand":brand,"model":model,"size":size,"smart":smart,"url":url})
+	collection.insert(doc)
 
+def read(fn):
+	# loading the sheet onto the memory
+	workbook = xlrd.open_workbook(fn)
+	# loading the first sheet from the workbook
+	worksheet = workbook.sheet_by_index(0)
+	for val in range(1,worksheet.nrows):
+		brand = (worksheet.cell(val,0).value)
+		model = (worksheet.cell(val,1).value)
+		size = (worksheet.cell(val,2).value)
+		smart = (worksheet.cell(val,3).value)
+		url  = (worksheet.cell(val,4).value)
+        print(brand)
+        print(model)
+        print(size)
+        print(smart)
+        print(url)
 
-for a in sheetnames:
-    sheet = wb[a]
-    with open('C:\\Users\sauravkhandelwal\Desktop\Scraper\LinksSAP.xlsx_'+ a +'.csv', 'w') as f:
-    c = csv.writer(f)
-    for row in sheet.rows:
-        sheet_value_arr.append([cell.value for cell in row])
-        for r in sheet.rows:
-            c.writerow([cell.value for cell in row])
+		# upload(brand, model, size, smart,url)
+	# print("Program upload complete")
 
-f.close()
+def main():
+	# enter the name of the sheet
+	fn = 'C:\\Users\sauravkhandelwal\Desktop\Scraper\LinksSAP.xlsx'
+	# calling the read functin responsible
+	read(fn)
+
+if __name__ == '__main__':
+	main()
