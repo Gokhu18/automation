414820197b3b37170b000d7384275a9aeb9a1fb0
Mono <mono@MonoBook.local>
2018-10-15 17:03:50 +0530

    Test find_url

--- url.py
+++ url.py
@@ -4,11 +4,9 @@ from pymongo import MongoClient
 #connect to database
 client = MongoClient('localhost',27017)
 #set the db object to pricetracker database
-db = client.pricetracker
-db = client[pricetracker]
+db = client['pricetracker']
 #set the db to TV collection
-collection = db.TV
-collection = db[TV]
+collection = db['TV']
 #result = collection.find()
-for str in collection.findAll():
-	print (str[url])
+for str in collection.find({}):
+	print(str['url'])
