cecacddcbb97c5970594a1c802ad712be7bd855e
Saurav Khandelwal <sauravkhandelwal@xiaomi.com>
2018-10-16 18:13:48 +0530

    updated scrap

--- scrap.py
+++ scrap.py
@@ -12,27 +12,26 @@ client = MongoClient('localhost',27017)
 db = client['pricetracker']
 #set the db to TV collection
 collection = db['TV']
+#Saving data to a excelsheet
+wb = Workbook()
+ws = wb.create_sheet("Sheet1",0)
+wb.save("price.xlsx")
+driver = webdriver.Chrome()
 for doc in collection.find({}):
 	# changed variable names
-	result = print(doc['url'])
-	driver = webdriver.Chrome()
-	#Saving data to a excelsheet
-	wb = Workbook()
-	ws = wb.create_sheet("Sheet1",0)
-	wb.save("price.xlsx")
+	result = doc['url']
 	#Total value there in database
-	val = doc('result')
 	xfile = openpyxl.load_workbook('price.xlsx')
 	sheet = xfile.get_sheet_by_name('Sheet1')
 	driver.get('result')
-	if (val):
+	if (result != null):
 		try:
 			#Getting the price
 			price = driver.find_element_by_xpath('//*[@id="container"]/div/div[1]/div[2]/div/div[1]/div[2]/div[2]/div/div[3]/div[1]/div/div[1]')
-		except Exception:
+		except TypeError:
 			print ("Some error in finding the price\t") + str(time.ctime())
 			pass
-			wb.save('price.xlsx')
+			wb.write_to_sheet('price.xlsx')
 		else:
 			print ("Some problem here at row: ") +str(row) + str(time.ctime())
 	print ("Data Scraped from url: ")+val
