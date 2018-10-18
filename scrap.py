89586f1911dd1acf42cb09aac5f5641b0da112ec
Saurav Khandelwal <sauravkhandelwal@xiaomi.com>
2018-10-18 10:39:42 +0530

    updated code with different implementation

--- scrap.py
+++ scrap.py
@@ -15,7 +15,8 @@ collection = db['TV']
 #Saving data to a excelsheet
 wb = Workbook()
 ws = wb.create_sheet("Sheet1",0)
-wb.save("price.xlsx")
+sheet = wb.active
+rows = sheet.rows
 driver = webdriver.Chrome()
 for doc in collection.find({}):
 	# changed variable names
@@ -23,18 +24,21 @@ for doc in collection.find({}):
 	#Total value there in database
 	xfile = openpyxl.load_workbook('price.xlsx')
 	sheet = xfile.get_sheet_by_name('Sheet1')
-	driver.get('result')
-	if (result != null):
+	driver.get(result)
+	if (result != 'null'):
 		try:
-			#Getting the price
-			price = driver.find_element_by_xpath('//*[@id="container"]/div/div[1]/div[2]/div/div[1]/div[2]/div[2]/div/div[3]/div[1]/div/div[1]')
+			for item in range(2,1,5):
+				#Getting the price
+				price = driver.find_element_by_xpath('//*[@id="container"]/div/div[1]/div[2]/div/div[1]/div[2]/div[2]/div/div['+item+']/div[1]/div/div[1]')
 		except TypeError:
-			print ("Some error in finding the price\t") + str(time.ctime())
-			pass
-			wb.save('price.xlsx')
+			print ("Some error in finding the price\t" + str(rows) + str(time.ctime()))
+			continue
 		else:
-			print ("Some problem here at db: ") +str(row) + str(time.ctime())
-	print ("Data Scraped from url: ")+val
+			print ("Some problem here at db: " + str(time.ctime()))
+	print ("Data Scraped from url: ")
+sheet.title = 'Data'
+sheet.append(price)
+wb.save('price.xlsx')	
 driver.quit()
 print ("Completed") 
 print (time.ctime())
