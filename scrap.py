733b4ae7f678f86742920e9abd998a21503e0f6d
Saurav Khandelwal <sauravkhandelwal@xiaomi.com>
2018-10-20 00:59:56 +0530

    Price Scraped

--- scrap.py
+++ scrap.py
@@ -1,6 +1,6 @@
 import selenium
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import *
 import time, openpyxl
 from openpyxl import Workbook
 import pymongo
@@ -12,39 +12,26 @@ client = MongoClient('localhost',27017)
 db = client['pricetracker']
 #set the db to TV collection
 collection = db['TV']
-#Saving data to a excelsheet
-wb = Workbook()
-ws = wb.create_sheet("Sheet1",0)
-sheet = wb.active
-rows = sheet.rows
 driver = webdriver.Chrome()
 for doc in collection.find({}):
 	# changed variable names
 	result = doc['url']
-	#Total value there in database
-	xfile = openpyxl.load_workbook('price.xlsx')
-	sheet = xfile.get_sheet_by_name('Sheet1')
-	driver.get(result)
-	if (result != 'null'):
-		try:
-			for item in range(2,1,5):
-				#Getting the price
-				price = driver.find_element_by_xpath('//*[@id="container"]/div/div[1]/div[2]/div/div[1]/div[2]/div[2]/div/div['+item+']/div[1]/div/div[1]')
-		except TypeError:
-			print ("Some error in finding the price\t" + str(rows) + str(time.ctime()))
-			continue
-		else:
-			print ("Some problem here at db: " + str(time.ctime()))
+	if( len(result) == 0):
+		print("NO DATA")
+	else:
+		driver.get(result)
+		price = driver.find_element_by_xpath('//*[@id="container"]/div/div[1]/div[2]/div/div[1]/div[2]/div[3]/div/div[3 or 4]/div[1]/div/div[1]')
+		print(price.text)
+		
+	#	try:
+	#		for item in range(2,1,5):
+	#			#Getting the price
+	#			price = driver.find_element_by_xpath('//*[@id="container"]/div/div[1]/div[2]/div/div[1]/div[2]/div[2]/div/div['+item+']/div[1]/div/div[1]')
+	#			print(price.text)
+	#	except Exception as er:
+	#		print(er)
+
 	print ("Data Scraped from url: ")
-sheet.title = 'Data'
-sheet.append(price)
-wb.save('price.xlsx')	
 driver.quit()
 print ("Completed") 
 print (time.ctime())
-
-# use None
-# if result == None:
-# 	pass
-# else:
-# 	fetch url and get the result
