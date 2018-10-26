d5c96d978ed58c68d7fb6188f73112cee4471eee
Saurav Khandelwal <sauravkhandelwal@xiaomi.com>
2018-10-26 15:24:36 +0530

    updated

--- log_controller.py
+++ log_controller.py
@@ -1,10 +1,16 @@
 import logging
 logger = logging.getLogger('program')
+# create file handler which logs even debug messages
 hdlr = logging.FileHandler('samilo.log')
+hdlr.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 hdlr.setFormatter(formatter)
+ch.setFormatter(formatter)
 logger.addHandler(hdlr)
-logger.setLevel(20)
+logger.setLevel(logging.DEBUG)
 
 def log_error(val):
 	# error files will be printed since are greater than warnings
